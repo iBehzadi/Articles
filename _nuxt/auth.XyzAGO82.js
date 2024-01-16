70a0b3c02b055270bfe064ac1de0dbf247f424e9
behzad sadeghi <bsd098@gmail.com>
2024-01-16 15:56:14 +0330

    Updates

---
+++ _nuxt/auth.XyzAGO82.js
@@ -0,0 +1,1 @@
+import{B as e}from"./entry.nwRfqwwf.js";const d=e((o,t)=>{console.log("middleware")});export{d as default};
--- assets/auth.y78PVKED.js
+++
@@ -1,1 +0,0 @@
-import{B as e}from"./entry.9SWchaTI.js";const d=e((o,t)=>{console.log("middleware")});export{d as default};
